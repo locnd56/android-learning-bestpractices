4afce8518694467378c65e7d54e958ed572f78d6
locnd56 <phoenician93@gmail.com>
2016-02-17 09:04:17 +0700

    tracking activity

--- app/src/main/java/com/example/exampleanalytics/MainActivity.java
+++ app/src/main/java/com/example/exampleanalytics/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.exampleanalytics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+
+import test.TestActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_1;
+    Button btn_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,25 +34,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initListener() {
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onRestart() {
+        super.onRestart();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CustomApplication.getInstance().trackScreen(this.getClass().getName());
     }
 }
--- app/src/main/java/test/TestActivity.java
+++ app/src/main/java/test/TestActivity.java
@@ -1,7 +1,23 @@
 package test;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.exampleanalytics.CustomApplication;
+
 /**
  * Created by Mr.Incredible on 2/16/2016.
  */
-public class TestActivity {
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_list_item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CustomApplication.getInstance().trackScreen(this.getClass().getName());
+    }
 }
