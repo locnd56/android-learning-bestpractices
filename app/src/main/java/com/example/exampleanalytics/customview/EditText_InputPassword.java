f8490a5eb7d3781826f8df856280640c6b2cde93
locnd_56 <phoenician93@gmail.com>
2016-02-22 16:43:55 +0700

    create Login Activity using text input layout library

---
+++ app/src/main/java/com/example/exampleanalytics/customview/EditText_InputPassword.java
@@ -0,0 +1,53 @@
+package com.example.exampleanalytics.customview;
+
+import android.app.Service;
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.example.exampleanalytics.R;
+
+/**
+ * Created by Mr.Incredible on 2/22/2016.
+ */
+public class EditText_InputPassword extends LinearLayout {
+    TextInputLayout text_inputlayout_password;
+    EditText edt_inputpassword;
+
+    public EditText_InputPassword(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+//        initData(context);
+    }
+
+    private void initView(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.edt_inputpassword, this);
+        edt_inputpassword = (EditText) findViewById(R.id.edt_inputpassword);
+        text_inputlayout_password = (TextInputLayout) findViewById(R.id.input_layout_password);
+    }
+
+    private void initData(Context context) {
+        validatePassword(context);
+    }
+
+    public boolean validatePassword(Context context) {
+        if (edt_inputpassword.getText().toString().trim().isEmpty()) {
+            text_inputlayout_password.setError(context.getString(R.string.err_msg_password));
+            edt_inputpassword.requestFocus();
+            return false;
+        } else {
+            text_inputlayout_password.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    public EditText getEdt_inputpassword() {
+        return edt_inputpassword;
+    }
+
+}
---
+++ app/src/main/java/com/example/exampleanalytics/utils/Utils.java
@@ -0,0 +1,53 @@
+package com.example.exampleanalytics.utils;
+
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.example.exampleanalytics.abstracts.AbstractFragment;
+
+/**
+ * Created by Mr.Incredible on 2/22/2016.
+ */
+public class Utils {
+    public static void hideKeyBoardWhenClickOutSide(final Activity activity, View view) {
+        if (!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(activity);
+                    return false;
+                }
+
+            });
+        }
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                hideKeyBoardWhenClickOutSide(activity, innerView);
+            }
+        }
+    }
+
+    public static View getCurrentView(Activity activity) {
+        if (activity instanceof Activity) {
+            return activity.getCurrentFocus();
+        } else {
+            AbstractFragment abstractFragment = new AbstractFragment();
+            return abstractFragment.getMainActivity().getCurrentFocus();
+        }
+
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+}
